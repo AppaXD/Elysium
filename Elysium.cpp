0df1c232dda3fafe151df2faf8fc0e7b166d16e5
AppaXD <appaxd@gmail.com>
2018-09-08 19:35:20 -0400

    Add files via upload

---
+++ Elysium.cpp
@@ -0,0 +1,108 @@
+#include "Elysium.hpp"
+#include <chrono>
+#include <iostream>
+
+void Elysium::init(unsigned short targetFPS, void(*onFrame)(), bool delay)
+{
+	float frametime = 1.f / targetFPS;
+
+	initscr();
+	start_color();
+	move(0, 0);
+
+	nodelay(stdscr, delay);
+
+	std::chrono::steady_clock::time_point start = std::chrono::high_resolution_clock::now();
+
+	while (1)
+	{
+		std::chrono::steady_clock::time_point end = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<double> diff = end - start;
+
+		if (diff.count() >= frametime)
+		{
+			start = std::chrono::high_resolution_clock::now();
+			clear();
+			onFrame();
+			refresh();
+		}
+	}
+}
+
+void Elysium::init(unsigned short targetFPS, void(*onFrame)(), void(*update)(), bool delay)
+{
+	float frametime = 1.f / targetFPS;
+
+	initscr();
+	start_color();
+	move(0, 0);
+
+	nodelay(stdscr, delay);
+
+	std::chrono::steady_clock::time_point start = std::chrono::high_resolution_clock::now();
+
+	while (1)
+	{
+		std::chrono::steady_clock::time_point end = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<double> diff = end - start;
+
+		update();
+
+		if (diff.count() >= frametime)
+		{
+			start = std::chrono::high_resolution_clock::now();
+			clear();
+			onFrame();
+			refresh();
+		}
+	}
+}
+
+void Elysium::print(std::string contents, int x, int y)
+{
+	move(y, x);
+	printw(contents.c_str());
+}
+
+void Elysium::print(std::string contents, rgb fgColour, int x, int y)
+{
+	fgColour.r *= 3.921;
+	fgColour.g *= 3.921;
+	fgColour.b *= 3.921;
+
+	init_color(8, fgColour.r, fgColour.g, fgColour.b);
+	init_pair(1, 8, COLOR_BLACK);
+	move(y, x);
+	attron(COLOR_PAIR(1));
+	printw(contents.c_str());
+}
+
+void Elysium::print(std::string contents, rgb fgColour, rgb bgColour, int x, int y)
+{
+	fgColour.r *= 3.921;
+	fgColour.g *= 3.921;
+	fgColour.b *= 3.921;
+
+	bgColour.r *= 3.921;
+	bgColour.g *= 3.921;
+	bgColour.b *= 3.921;
+
+	init_color(8, fgColour.r, fgColour.g, fgColour.b);
+	init_color(9, bgColour.r, bgColour.g, bgColour.b);
+	init_pair(1, 8, 9);
+	move(y, x);
+	attron(COLOR_PAIR(1));
+	printw(contents.c_str());
+}
+
+Elysium::Rand::Rand()
+{
+	this->seed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+	this->gen = std::mt19937(seed);
+}
+
+int Elysium::Rand::range(int min, int max)
+{
+	std::uniform_int_distribution<int> rng(min, max);
+	return rng(gen);
+}
---
+++ Elysium.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <curses.h>
+#include <string>
+#include <random>
+
+namespace Elysium
+{
+	class rgb
+	{
+	public:
+		int r, g, b;
+		rgb(int r, int g, int b)
+		{
+			this->r = r;
+			this->g = g;
+			this->b = b;
+		}
+	};
+
+	class vec2
+	{
+	public:
+		int x, y;
+		vec2(int x, int y)
+		{
+			this->x = x;
+			this->y = y;
+		}
+	};
+
+	class Rand
+	{
+	private:
+		long long seed;
+		std::mt19937 gen;
+	public:
+		Rand();
+		int range(int min, int max);
+	};
+
+	namespace Colours
+	{
+		static rgb Black(0, 0, 0);
+		static rgb White(255, 255, 255);
+		static rgb Red(255, 0, 0);
+		static rgb Green(0, 255, 0);
+		static rgb Blue(0, 0, 255);
+		static rgb Yellow(255, 255, 0);
+		static rgb Cyan(0, 255, 255);
+		static rgb Magenta(255, 0, 255);
+	}
+
+	void init(unsigned short targetFPS, void (*onFrame)(), bool delay);
+	void init(unsigned short targetFPS, void(*onFrame)(), void (*update)(), bool delay);
+	void print(std::string contents, int x, int y);
+	void print(std::string contents, rgb fgColour, int x, int y);
+	void print(std::string contents, rgb fgColour, rgb bgColour, int x, int y);
+}
